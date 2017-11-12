ecc03e1087693f400fd3311ddc2896d4a9f96d10
skmbw <tongku2008@126.com>
2017-11-12 12:50:49 +0800

    用modelandview还是 转发请求进入filter

--- src/main/java/com/hellokoding/springboot/HelloController.java
+++ src/main/java/com/hellokoding/springboot/HelloController.java
@@ -4,12 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
+
     @RequestMapping("/hello")
-    public String hello(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
+    public ModelAndView hello(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
         model.addAttribute("name", name);
-        return "hello";
+        ModelAndView mv = new ModelAndView("hello");
+        mv.addObject("name", name);
+        return mv;
     }
 }
